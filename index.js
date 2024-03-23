6ad5060f9e20c6ae62f6e63acbb258a5acab6a4f
Tobi <tobiawolaju21@gmail.com>
2024-03-23 10:42:08 +0100

    vuigcsz

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
+
 const express = require('express');
-const axios = require('axios');
 const app = express();
+const authKey = 'c0a7daee-7756-eba3-f1cf-6e1ba124ef6d'; // Replace [yourAuthKey] with your actual DeepL API key
 const PORT = process.env.PORT || 8081;
+
 
 app.get('/', async (req, res) => {
   const username = req.query.username || 'myogeshchavan97';
-  try {
-    const result = await axios.get(
-      `https://api.github.com/users/${username}/repos`
-    );
-    const repos = result.data
-      .map((repo) => ({
-        name: repo.name,
-        url: repo.html_url,
-        description: repo.description,
-        stars: repo.stargazers_count
-      }))
-      .sort((a, b) => b.stars - a.stars);
 
-    res.send(repos);
-  } catch (error) {
-    res.status(400).send('Error while getting list of repositories');
-  }
+  const translateText = async () => {
+    try {
+      const response = await axios.post('https://api.deepl.com/v2/translate', {
+        text: ["Hello, world! my guys"],
+        target_lang: "DE"
+      }, {
+        headers: {
+          'Authorization': `DeepL-Auth-Key ${authKey}`,
+          'Content-Type': 'application/json'
+        }
+      });
+
+      console.log(response.data.translations[0].text);
+    } catch (error) {
+      console.error('Error translating text:', error.message);
+    }
+  };
+
+  
+translateText();
+
 });
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
+
